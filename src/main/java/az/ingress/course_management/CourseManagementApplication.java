6c6d797d4f29e413908a12f7b8f275a56464d0bc
Mahammad Baloghlanov <mehemmedbaloglanov300@gmail.com>
2024-08-25 12:23:16 +0400

    lesson 9

---
+++ src/main/java/az/ingress/course_management/CourseManagementApplication.java
@@ -0,0 +1,13 @@
+package az.ingress.course_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseManagementApplication.class, args);
+	}
+
+}
